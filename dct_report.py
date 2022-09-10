0d4eef36193dbdc8d899f40b99122db91332a9b9
mattdelphix <matteo.ferrari@delphix.com>
2022-09-11 00:53:21 +0200

    Initial version

--- dct_report.py
+++ dct_report.py
@@ -1,9 +1,100 @@
+#
+# dct_report
+#
+
 import argparse
 from datetime import datetime
-
-from reports import *
+from helpers import *
 
 # TODO add sort and limit to all reports as parameter
+
+# Report functions
+def report_api_usage(begin, end):
+    date_begin = urllib.parse.quote(begin)
+    date_end = urllib.parse.quote(end)
+    resp = url_GET("/reporting/api-usage-report?start_date=" + date_begin + "&end_date=" + date_end)
+    if resp.status_code == 200:
+        report_data = resp.json()['items']
+        if report_data:
+            tabular_report(f"DELPHIX Data Control Tower - API USAGE REPORT\nInterval: {begin} to {end}", report_data)
+        else:
+            print(f"\nNo API Usage Report for this interval - {begin} to {end}")
+            return
+    else:
+        print(f"ERROR: Status = {resp.status_code} - {resp.text}")
+        sys.exit(1)
+
+
+def report_product_info():
+    resp = url_GET("/reporting/product_info")
+    if resp.status_code == 200:
+        tabular_report("DELPHIX Data Control Tower - PRODUCT INFO", resp.json())
+    else:
+        print(f"ERROR: Status = {resp.status_code} - {resp.text}")
+        sys.exit(1)
+
+
+def report_vdb_inventory(report_filter):
+    display_filter = ""
+    if report_filter is not None:
+        payload = {"filter_expression": report_filter}
+        display_filter = "SEARCH: "+report_filter
+        resp = url_POST("/reporting/vdb-inventory-report/search", payload)
+    else:
+        resp = url_GET("/reporting/vdb-inventory-report")
+    if resp.status_code == 200:
+        report_data = resp.json()['items']
+        if report_data:
+            tabular_report("DELPHIX Data Control Tower - DSource Usage Report " + display_filter, report_data)
+            return report_data
+        else:
+            print(f"\nNo DSources match search criteria.")
+            return
+    else:
+        print(f"ERROR: Status = {resp.status_code} - {resp.text}")
+        sys.exit(1)
+
+
+def report_dsource_usage(report_filter):
+    display_filter = ""
+    if report_filter is not None:
+        payload = {"filter_expression": report_filter}
+        display_filter = "SEARCH: "+report_filter
+        resp = url_POST("/reporting/dsource-usage-report/search", payload)
+    else:
+        resp = url_GET("/reporting/dsource-usage-report")
+    if resp.status_code == 200:
+        report_data = resp.json()['items']
+        if report_data:
+            tabular_report("DELPHIX Data Control Tower - DSource Usage Report " + display_filter, report_data)
+            return report_data
+        else:
+            print(f"\nNo DSources match search criteria.")
+            return
+    else:
+        print(f"ERROR: Status = {resp.status_code} - {resp.text}")
+        sys.exit(1)
+
+
+def report_storage_summary(report_filter):
+    display_filter = ""
+    if report_filter is not None:
+        payload = {"filter_expression": report_filter}
+        display_filter = "SEARCH: "+report_filter
+        resp = url_POST("/reporting/virtualization-storage-summary-report/search", payload)
+    else:
+        resp = url_GET("/reporting/virtualization-storage-summary-report")
+    if resp.status_code == 200:
+        report_data = resp.json()['items']
+        if report_data:
+            tabular_report("DELPHIX Data Control Tower - Storage summary Report " + display_filter, report_data)
+            return report_data
+        else:
+            print(f"\nNo Delphix Engines match search criteria.")
+            return
+    else:
+        print(f"ERROR: Status = {resp.status_code} - {resp.text}")
+        sys.exit(1)
 
 parser = argparse.ArgumentParser(description = "Delphix DCT Reports")
 subparser = parser.add_subparsers(dest='command')
@@ -21,7 +112,6 @@ product_info = subparser.add_parser('product_info')
 # Get current date time in ISO FORMAT
 #TODO to be implemented logic to set current time as API end timestamp
 end_time = datetime.now().isoformat()
-#print(end_time)
 
 # define api_usage parms
 api_usage.add_argument('--begin', type=str, required=False, help="begin timestamp for report", default='2020-01-01T15:00:00-00:00')
@@ -39,6 +129,7 @@ dsource_usage.add_argument('--filter', type=str, required=False, help="DSource U
 # force help if no parms
 args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
+# Start processing
 if args.command == 'api_usage':
     rs = report_api_usage(args.begin, args.end)
 
