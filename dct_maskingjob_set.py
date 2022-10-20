2d059ede632d1c2363a0c7103d2a8fc9cf53eafd
Ruben Catarrunas <ruben.catarrunas@delphix.com>
2022-10-20 13:15:18 +0200

    Ruben - To dct_maskingjob_set.py       - Added functions to update and create tags.

--- dct_maskingjob_set.py
+++ dct_maskingjob_set.py
@@ -5,6 +5,18 @@
 import argparse
 from helpers import *
 
+def maskingjob_set_update(base_url, maskingjob_set_id, name):
+    payload = {"name": name}
+
+    resp = url_PATCH(base_url + "/" + urllib.parse.quote(maskingjob_set_id), payload)
+    if resp.status_code == 200:
+        rsp = resp.json()
+        print("Updated Maskingjob_Set" + " - ID=" + maskingjob_set_id)
+        return rsp
+    else:
+        print(f"ERROR: Status = {resp.status_code} - {resp.text}")
+        sys.exit(1)
+
 # Init
 parser = argparse.ArgumentParser(description="Delphix DCT Masking job set operations")
 subparser = parser.add_subparsers(dest='command')
@@ -16,6 +28,8 @@ parser.add_argument('--config', type=str, required=False, help="Config file")
 lst = subparser.add_parser('list')
 search = subparser.add_parser('search')
 view = subparser.add_parser('view')
+update = subparser.add_parser('update')
+tag_create = subparser.add_parser('tag_create')
 tag_list = subparser.add_parser('tag_list')
 tag_delete = subparser.add_parser('tag_delete')
 tag_delete_all = subparser.add_parser('tag_delete_all')
@@ -31,6 +45,14 @@ view.add_argument('--id', type=str, required=True, help="Masking job set ID to b
 search.add_argument('--filter', type=str, required=False, help="Masking Job Set search string")
 search.add_argument('--format', type=str, required=False, help="Type of output", choices=['json', 'report'])
 
+# define update parms
+update.add_argument('--id', type=str, required=True, help="Masking job set ID to be updated")
+update.add_argument('--name', type=str, required=True, help="Masking job set name to be updated")
+
+# define tag_create params
+tag_create.add_argument('--id', type=str, required=True, help="Masking job set ID to add tags to")
+tag_create.add_argument('--tags', nargs='*', type=str, required=True, action=dct_parsetags,
+                        help="Tags of the DSource in this format:  key=value key=value")
 # define tag_list parms
 tag_list.add_argument('--id', type=str, required=True, help="Masking Job Set ID for tags list")
 tag_list.add_argument('--format', type=str, required=False, help="Type of output", choices=['json', 'report'])
@@ -66,6 +88,18 @@ if args.command == 'search':
     rs = dct_search("Masking job set List", dct_base_url, args.filter, "No Masking Job Sets match the search criteria.",
                     args.format)
     print(rs)
+
+if args.command == "update":
+    maskingjob_set_update (dct_base_url,args.id ,args.name)
+
+if args.command == 'tag_create':
+    payload = {"tags": args.tags}
+    rs = dct_post_by_id(dct_base_url, args.id, payload, "tags")
+    if rs.status_code == 201:
+        print("Create tags for Account - ID=" + args.id)
+    else:
+        print(f"ERROR: Status = {rs.status_code} - {rs.text}")
+        sys.exit(1)
 
 if args.command == 'tag_list':
     rs = dct_list_by_id(dct_base_url, args.id, "/tags", args.format)
