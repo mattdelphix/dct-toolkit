6a1b615c0eb9e496a26d8a58891466312c76c825
Ruben Catarrunas <ruben.catarrunas@delphix.com>
2022-10-26 18:13:22 +0200

    Ruben - Added new functions for remove-job and copy

--- dct_maskingjob_set.py
+++ dct_maskingjob_set.py
@@ -17,6 +17,26 @@ def maskingjob_set_update(base_url, maskingjob_set_id, name):
         print(f"ERROR: Status = {resp.status_code} - {resp.text}")
         sys.exit(1)
 
+def maskingjob_set_copy(base_url, name, maskingjob_set_id, target_engine_id, source_engine_id, source_environment_id, target_environment_id):
+    if source_environment_id is not None:
+        payload = {"target_engine_id": target_engine_id,
+                   "source_engine_id": source_engine_id,
+                   "environment_id": target_environment_id}
+    else:
+        payload = {"target_engine_id": target_engine_id,
+                   "source_engine_id": source_engine_id,
+                   "environment_id": target_environment_id,
+                   "source_environment_id": source_environment_id}
+
+    resp = url_POST(base_url + "/" + urllib.parse.quote(maskingjob_set_id) + "/copy", payload)
+    if resp.status_code == 200:
+        rsp = resp.json()
+        print("Masking Job copied " + maskingjob_set_id + " - ID to Engine " + target_engine_id + " for environment " + target_environment_id)
+        return rsp
+    else:
+        print(f"ERROR: Status = {resp.status_code} - {resp.text}")
+        sys.exit(1)
+
 # Init
 parser = argparse.ArgumentParser(description="Delphix DCT Masking job set operations")
 subparser = parser.add_subparsers(dest='command')
@@ -34,6 +54,8 @@ tag_list = subparser.add_parser('tag_list')
 tag_delete = subparser.add_parser('tag_delete')
 tag_delete_all = subparser.add_parser('tag_delete_all')
 connector_list = subparser.add_parser('connector_list')
+copy = subparser.add_parser('copy')
+remove_job = subparser.add_parser('remove_job')
 
 # define list parms
 lst.add_argument('--format', type=str, required=False, help="Type of output", choices=['json', 'report'])
@@ -68,6 +90,17 @@ tag_delete_all.add_argument('--id', type=str, required=True, help="Account ID to
 connector_list.add_argument('--id', type=str, required=True, help="Masking Job Set ID to be viewed")
 connector_list.add_argument('--engine_id', type=str, required=True, help="Continuous Compliance engine ID to be viewed")
 connector_list.add_argument('--format', type=str, required=False, help="Type of output",  choices=['json', 'report'])
+
+# define connector_list parms
+copy.add_argument('--id', type=str, required=True, help="Masking Job Set ID to be viewed")
+copy.add_argument('--source_engine_id', type=str, required=True, help="Continuous Compliance engine ID to be copied from")
+copy.add_argument('--target_engine_id', type=str, required=True, help="Continuous Compliance engine ID to receive job")
+copy.add_argument('--source_environment_id', type=str, required=False, help="Continuous Compliance environment ID or Name to be copied from")
+copy.add_argument('--target_environment_id', type=str, required=True, help="Continuous Compliance environment ID or Name receive job")
+
+# define remove-job params
+remove_job.add_argument('--id', type=str, required=True, help="Masking Job Set ID to delete tags from")
+remove_job.add_argument('--engine_id', type=str, required=True, help="Continuous Compliance engine ID for Job set to be removed")
 
 # force help if no parms
 args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
@@ -126,3 +159,21 @@ if args.command == 'tag_delete_all':
 if args.command == 'connector_list':
     rs = dct_list_by_id(dct_base_url, args.id, "/connectors?engine_id="+args.engine_id, args.format)
     print(rs)
+
+if args.command == 'copy':
+    if args.source_environment_id is not None:
+        payload = {"target_engine_id":  args.target_engine_id,
+                   "source_engine_id":  args.source_engine_id,
+                   "environment_id":  args.target_environment_id}
+    else:
+        payload = {"target_engine_id":  args.target_engine_id,
+                   "source_engine_id":  args.source_engine_id,
+                   "environment_id":  args.target_environment_id,
+                   "source_environment_id":  args.source_environment_id}
+
+    rs = dct_post_by_id(dct_base_url, args.id, payload, "copy")
+
+
+if args.command == 'remove_job':
+    payload = {"engine_id": args.engine_id}
+    rs = dct_post_by_id(dct_base_url, args.id, payload, "remove-job")
