a1dbe49f6668755889060ece6c7fd76c06feff43
Ruben Catarrunas <ruben.catarrunas@delphix.com>
2022-11-29 12:33:51 +0100

    Ruben - Added class for cdbs - Added class for vcdbs

---
+++ dct_cdbs.py
@@ -0,0 +1,64 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright (c) 2022 by Delphix. All rights reserved.
+#
+# Author  : Matteo Ferrari, Ruben Catarrunas
+# Date    : September 2022
+
+
+from helpers import *
+
+# Init
+parser = argparse.ArgumentParser(description='Delphix DCT CDBs operations')
+subparser = parser.add_subparsers(dest='command')
+
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+parser.add_argument('--config', type=str, required=False, help="Config file")
+
+# define commands
+
+lst = subparser.add_parser('list')
+search = subparser.add_parser('search')
+view = subparser.add_parser('view')
+
+# define view parms
+view.add_argument('--id', type=str, required=True, help="CDB to be viewed")
+
+# define list parms
+lst.add_argument('--format', type=str, required=False, help="Type of output",  choices=['json', 'report'])
+
+# define search parms
+search.add_argument('--filter', type=str, required=False, help="CDB search string")
+search.add_argument('--format', type=str, required=False, help="Type of output",  choices=['json', 'report'])
+
+# force help if no parms
+args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+# Start processing
+dct_read_config(args.config)
+
+dct_base_url = "/cdbs"
+
+if args.command == 'list':
+    rs = dct_search("CDBs List ", dct_base_url, None, "No CDBs defined.", args.format)
+    print(rs)
+
+if args.command == 'view':
+    rs = dct_view_by_id(dct_base_url, args.id)
+    print(rs)
+
+if args.command == 'search':
+    rs = dct_search("CDBs List ", dct_base_url, args.filter, "No CDBs match the search criteria.",
+                    args.format)
+    print(rs)
